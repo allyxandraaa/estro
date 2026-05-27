d681440fd4e798d46371cf4858a99bd8b60a9da8
allyxandraaa <minelenova1@gmail.com>
2026-05-27 19:33:23 +0100

    feat: add animated cell field background to auth pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import CellField from './components/CellField.vue'
 </script>
 
 <template>
+  <CellField :density="24" :palette="['ink', 'moss']" :intensity="0.7" />
   <RouterView />
 </template>
