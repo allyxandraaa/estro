1c2c1168fbeb7e4eb8017a1b1f51b322d8dec4dc
Dariya Omelchenko <dariyaomelchenko25@gmail.com>
2026-05-28 00:33:28 +0200

    fix: async session, add alembic migrations

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
@@ -12,9 +14,6 @@ class Base(DeclarativeBase):
     pass
 
 
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
-        try:
-            yield session
-        finally:
-            await session.close()
+        yield session
